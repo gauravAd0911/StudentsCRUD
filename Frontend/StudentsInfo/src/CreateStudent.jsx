5ef2e0d6d2909408e1ac29c534ae8feb2dff7bf3
Gaurav Adulkar <103197164+gauravAd0911@users.noreply.github.com>
2024-03-27 09:11:41 +0530

    Update CreateStudent.jsx

--- Frontend/StudentsInfo/src/CreateStudent.jsx
+++ Frontend/StudentsInfo/src/CreateStudent.jsx
@@ -3,37 +3,38 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 
-
-
 const CreateStudent = () => {
 
-    const [name,setName]=useState(' ')
-    const [email,setEmail]=useState(' ')
+    const [name,setName]=useState('')
+    const [email,setEmail]=useState('')
     const navigate=useNavigate();
+
 
     function handleSubmit(event) {
         event.preventDefault()
-        axios.post('http://localhost:5000/create',{name,email})
+        axios.post(`http://localhost:5000/create`,{Name:name,Email:email})
         .then(res=>{
-         console.log(res);  
-         navigate('/');
+            console.log(res);  
+            navigate('/');
         })
-        .catch(err=>console.log(err));
+        .catch(error=>console.log(error));
     }
+
+
 
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-        <div className='w-50 bg-white rounded p-3'>
+        <div className='w-50 bg-dark text-white rounded p-3'>
             <form onSubmit={handleSubmit}>
                 <h2>Add Students</h2>
                 <div className='mb-2'>
-                    <label htmlFor="">Name</label>
-                    <input type="text" placeholder='Enter Name' className='form-control' 
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id='name' placeholder='Enter Name' className='form-control' 
                     onChange={e=>setName(e.target.value)}/>
                 </div>
                 <div className='mb-2'>
-                    <label htmlFor="">Email</label>
-                    <input type="email" placeholder='Enter Email' className='form-control'
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' placeholder='Enter Email' className='form-control'
                      onChange={e=>setEmail(e.target.value)}/>
                 </div>
                 <button className='btn btn-success'>Submit</button>
